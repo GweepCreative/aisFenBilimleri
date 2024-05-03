baa1f5e52d990e9062860296d470e9fae23942b5
GweepCreative <gweepcreative@gmail.com>
2024-05-03 19:12:01 +0300

    Frontend: AIS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,181 @@
-import Image from 'next/image'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const sonuclar = [
+  {
+    dersAdı: "Türkçe",
+    soruSayısı: 40,
+    doğru: 21,
+    yanlış: 3,
+    bos: 16,
+    net: 20,
+  },
+  {
+    dersAdı: "Tarih",
+    soruSayısı: 5,
+    doğru: 4,
+    yanlış: 1,
+    bos: 0,
+    net: 3.75,
+  },
+  {
+    dersAdı: "Coğrafya",
+    soruSayısı: 5,
+    doğru: 4,
+    yanlış: 1,
+    bos: 0,
+    net: 3.75,
+  },
+  {
+    dersAdı: "Felsefe",
+    soruSayısı: 5,
+    doğru: 4,
+    yanlış: 1,
+    bos: 0,
+    net: 3.75,
+  },
+  {
+    dersAdı: "Din Kültürü ve Ahlak Bilgisi",
+    soruSayısı: 5,
+    doğru: 4,
+    yanlış: 1,
+    bos: 0,
+    net: 3.75,
+  },
+  {
+    dersAdı: "Felsefe (2)",
+    soruSayısı: 5,
+    doğru: 4,
+    yanlış: 1,
+    bos: 0,
+    net: 3.75,
+  },
+  {
+    dersAdı: "Temel Matematik",
+    soruSayısı: 40,
+    doğru: 21,
+    yanlış: 3,
+    bos: 16,
+    net: 20,
+  },
+  {
+    dersAdı: "Fizik",
+    soruSayısı: 7,
+    doğru: 6,
+    yanlış: 1,
+    bos: 9,
+    net: 5.75,
+  },
+  {
+    dersAdı: "Kimya",
+    soruSayısı: 7,
+    doğru: 6,
+    yanlış: 1,
+    bos: 9,
+    net: 5.75,
+  },
+  {
+    dersAdı: "Biyoloji",
+    soruSayısı: 6,
+    doğru: 5,
+    yanlış: 1,
+    bos: 0,
+    net: 4.75,
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center  p-24 ">
+      <div className="flex flex-col gap-y-2 justify-center items-center">
+        <h1 className="font-extrabold sm:text-4xl text-xl">FEN BİLİMLERİ KOLEJİ</h1>
+        <h6 className="sm:text-2xl text-lg font-medium">Bursluluk Sonuç Sistemi</h6>
+      </div>
+
+      <div className="justify-center items-center flex flex-col mt-20 gap-y-20">
+        <div className="flex flex-row justify-center items-center gap-x-5">
+          <Avatar className="w-24 h-24 rounded-xl">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col justify-center items-start space-y-1">
+            <h1 className="font-extrabold text-3xl">Çağatay Nalcı</h1>
+            <h6 className="font-semibold">6. SINIF</h6>
+            <h6 className="text-sm font-medium">03/10/2006</h6>
+          </div>
+        </div>
+
+        <div className="flex space-y-10 flex-col justify-center items-center">
+         
+
+          <div className="flex flex-col space-y-4 w-full px-8">
+            <div className="flex flex-row space-x-4">
+              <div className="flex flex-col items-center justify-center p-2 bg-green-500 rounded-lg text-white">
+                <h1 className="text-center">Toplam Doğru</h1>
+                <h2 className="text-center font-bold">100</h2>
+              </div>
+
+              <div className="flex flex-col items-center justify-center p-2 bg-red-500 rounded-lg text-white">
+                <h1 className="text-center">Toplam Yanlış</h1>
+                <h2 className="text-center font-bold">100</h2>
+              </div>
+
+              <div className="flex flex-col items-center justify-center p-2 bg-zinc-400 rounded-lg text-white">
+                <h1 className="text-center">Toplam Boş</h1>
+                <h2 className="text-center font-bold">100</h2>
+              </div>
+
+              <div className="flex flex-col items-center justify-center p-2 bg-blue-500 rounded-lg text-white">
+                <h1 className="text-center">Toplam Net</h1>
+                <h2 className="text-center font-bold">100</h2>
+              </div>
+            </div>
+            <div className="w-full bg-zinc-200 rounded-md p-2 flex flex-col items-center justify-center">
+              <h1 className="text-2xl">Puanı</h1>
+              <h2 className="text-2xl font-bold">400,23</h2>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <Table>
+              <TableCaption>
+                20/06/2024 Tarihli bursluluk sınavı sonuçları
+              </TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="font-bold text-xs sm:text-sm">Dersler</TableHead>
+                  <TableHead className="font-bold text-xs sm:text-sm">SS</TableHead>
+                  <TableHead className="font-bold text-xs sm:text-sm">Doğru</TableHead>
+                  <TableHead className="font-bold text-xs sm:text-sm">Yanlış</TableHead>
+                  <TableHead className="font-bold text-xs sm:text-sm">Boş</TableHead>
+                  <TableHead className="font-bold text-xs sm:text-sm">Net</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {sonuclar.map((ders, index) => (
+                  <TableRow key={index}>
+                    <TableCell className="font-bold">{ders.dersAdı}</TableCell>
+                    <TableCell className="text-center text-xs sm:text-sm">{ders.soruSayısı}</TableCell>
+                    <TableCell className="text-center text-xs sm:text-sm">{ders.doğru}</TableCell>
+                    <TableCell className="text-center text-xs sm:text-sm">{ders.yanlış}</TableCell>
+                    <TableCell className="text-center text-xs sm:text-sm">{ders.bos}</TableCell>
+                    <TableCell className="text-center text-xs sm:text-sm">{ders.net}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
